857b8f2752de37ef800397f3168105c2b4bfafee
zawzawzaw <zawzawzaw@gmail.com>
2015-12-20 23:45:15 +0800

    updated masonary

--- html/04/js/case-study.js
+++ html/04/js/case-study.js
@@ -1,18 +1,27 @@
 $(function() {
 
-	$('.grid').masonry({
-	  itemSelector: '.grid-item',
-	  columnWidth: '.grid-sizer',
-	  gutter: '.gutter-sizer'
+	$('.grid').isotope({
+	  	itemSelector: '.grid-item',
+	  	masonry: {
+		  	columnWidth: '.grid-sizer',
+		  	gutter: '.gutter-sizer'
+		}
 	});
 
 	// ,
 	//   gutter: '.gutter-sizer'
 
-	$('.grid-2').masonry({
-	  itemSelector: '.grid-item-2',
-	  columnWidth: '.grid-sizer-2',
-	  gutter: '.gutter-sizer-2'
+	$('.grid-2').isotope({
+	  	itemSelector: '.grid-item-2',
+	  	percentPosition: true,
+	  	getSortData: {	    	
+	    	number: '.number parseInt'
+	  	},
+	  	sortBy: 'number',
+	  	masonry: {
+		  	columnWidth: '.grid-sizer-2',
+		  	gutter: '.gutter-sizer-2'
+		}
 	});	
 
 	$(window).resize(function(){
