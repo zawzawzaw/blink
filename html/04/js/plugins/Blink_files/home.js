bf3a90bb2db0515f24b1b7a95dc387771885769a
zawzawzaw <zawzawzaw@gmail.com>
2015-12-23 16:03:12 +0800

    updated

---
+++ html/04/js/plugins/Blink_files/home.js
@@ -0,0 +1,68 @@
+$(function() {
+
+	function init () {
+
+		var scrollMagicController = new ScrollMagic();
+
+		var $eachContentDiv = $('.each-content'),
+			$rightImage = $('.img-right-content img'),
+			$leftImage = $('.img-left-content img'),
+			$rightText = $('.text-right-content .v-align-table'),
+			$leftText = $('.text-left-content .v-align-table'),
+			$headingDiv = $('#heading');
+
+		// $rightImage.css({ 'position':'absolute', 'left': '2000px' });
+		// $leftImage.css({ 'position':'absolute', 'right': '2000px' });
+		// $rightText.css({ 'position':'absolute', 'left': '2000px' });
+		// $leftText.css({ 'position':'absolute', 'right': '2000px' });
+
+		var headingTxtTween = TweenLite.from($headingDiv, 0.8, { scale: 0.2, opacity: 0, top: "300px", ease:Back.easeOut }); // bring in text;
+
+		var scene = new ScrollScene({
+			    triggerElement: $headingDiv,
+			    triggerHook: 'onEnter',
+			    duration: 0 /* How many pixels to scroll / animate */
+		  	})
+		  	.setTween(headingTxtTween)
+		  	.addTo(scrollMagicController);
+
+		// $('.each-content').each(function(i, obj){					
+			
+		// 	var rightImgTween = TweenMax.to($(obj).find('.img-right-content img'), 2, {
+		// 		css: { top: 0, left: 0, opacity: 1 }
+		// 	});
+
+		// 	var leftImgTween = TweenMax.to($(obj).find('.img-left-content img'), 2, {
+		// 		css: { top: 0, right: 0, opacity: 1 }
+		// 	});
+
+		// 	var rightTxtTween = TweenMax.to($(obj).find('.text-right-content .v-align-table'), 2, {
+		// 		css: { top: 0, left: 0, opacity: 1 }
+		// 	});
+
+		// 	var leftTxtTween = TweenMax.to($(obj).find('.text-left-content .v-align-table'), 2, {
+		// 		css: { top: 0, right: 0, opacity: 1 }
+		// 	});
+
+		// 	var tl = new TimelineMax({delay:0.5, repeat:0});
+
+		// 	tl.add([rightImgTween, leftImgTween, rightTxtTween, leftTxtTween]);
+
+		// 	var scene = new ScrollScene({
+		// 	    triggerElement: obj,
+		// 	    triggerHook: 'onEnter', 	
+		// 	    triggerOffset: 0,				    
+		// 	    duration: 300 /* How many pixels to scroll / animate */
+		//   	})
+		//   	.setTween(tl)
+		//   	.addTo(scrollMagicController);
+
+		  	
+	 //  	});
+
+	  	// scene.addIndicators();
+	}
+
+	init();
+  
+});
--- html/05/js/portfolio.js
+++ html/05/js/portfolio.js
@@ -3,8 +3,7 @@ $(function(){
 		$filterCategory = $('.filter-category'),
 		$filterTitle = $('.filtered-title'),
 		$childMenu = $('.child-menu'),
-		$portfolioMenuItem = $('.portfolio-sub-nav ul li a');
-
+		$portfolioMenuItem = $('.portfolio-sub-nav ul li a');		
 
 	$filter.on('click', function(e){
 		e.preventDefault();
@@ -13,12 +12,16 @@ $(function(){
 
 	$filterCategory.on('click', function(e){
 		e.preventDefault();
-		$('.active').removeClass('active');
-		$(this).addClass('active')
+		$('.category-nav').find('.active').removeClass('active');
+		$(this).addClass('active');
 
-		if($(this).attr('href')=='#all'){
+		var hash = $(this).attr('href');		
+
+		if(hash=='#all' || hash=="#signaturebyblink" || hash == "#"){
+			$('#default-radio').trigger('click');
 			$filterTitle.hide();
 		}else {
+			$(hash).trigger('click');
 			$filterTitle.show();
 			$filterTitle.text($(this).text());
 		}
@@ -29,13 +32,28 @@ $(function(){
 
 		changeFilterText(url);
 		
-	})
+	});
+
+	$('.show-more').on('click', function(e){
+		var html = '<li class="list-item"><div class="hotels"><div class="img-holder"><img src="images/content/portfolio/portfolio-content-1.jpg" alt=""></div><div class="caption-text"><h6>NEW</h6><h5>New Hotels</h5></div></div></li>',
+			$el = $(html);
+		
+		//add item to jplist collection (as last item)
+	   	$('#category').jplist({
+	      	command: 'add',
+	      	commandData: {
+	        	$item: $el
+	      	}
+	   	});
+
+	});
 
 
 	/////
 	/////
 
 	function findTextFromHash(hash) {	
+		$('.category-nav').find('.active').removeClass('active');
 		return $childMenu.find("a[data-hash='" + hash + "']").addClass('active').text();
 	}
 
@@ -51,8 +69,10 @@ $(function(){
 		var hash = getHashFromUrl(url);
 
 		if(hash=='#all' || hash=="#signaturebyblink" || hash == "#"){
+			$('#default-radio').trigger('click');
 			$filterTitle.hide();
 		}else {
+			$(hash).trigger('click');
 			$filterTitle.show();
 			var filtered_title = findTextFromHash(hash);
 			$filterTitle.text(filtered_title);
@@ -60,5 +80,15 @@ $(function(){
 	}
 
 	changeFilterText();
+
+	///////
+	///////   	
+
+	$('#category').jplist({				
+		itemsBox: '.list',
+		itemPath: '.list-item',
+		panelPath: '.jplist-panel',
+		effect: 'fade'
+	});
 	
 });
